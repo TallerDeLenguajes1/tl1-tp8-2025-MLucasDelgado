7e940f4ac9325b13ab1d0ffcf56074ee3187e0c7
MLucasDelgado <lucasdelgado1011@gmail.com>
2025-06-06 00:27:34 -0300

    Clase tarea con sus propiedades

--- ToDo/Tarea.cs
+++ ToDo/Tarea.cs
@@ -1,7 +1,10 @@
 namespace Tareas {
-    public class Tarea {
-    public int TareaID {get; set;}
-    public string Description {get; set;}
-    public int Duracion {get; set;}
+    public class Tarea
+    {
+        public int TareaID;
+        public string Description { get; set; }
+        public int Duracion { get; set; }
+    
+    public int IDTarea { get => TareaID; set => TareaID = value; } // otra forma de hacerlo
 }
 }
